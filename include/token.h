8ef1051fee93e60fdb8eead7f24882e7a6105d82
doraemonext <doraemonext@gmail.com>
2016-01-04 03:16:57 +0800

    Fix: 修正文件目录结构

--- include/token.h
+++ include/token.h
@@ -53,58 +53,23 @@ public:
     };
 
 public:
-    Token(const Type &type, const std::string &content, const TokenPosition &position) :
-            type_(type), content_(content), position_(position) { }
+    Token(const Type &type, const std::string &content, const TokenPosition &position) : type_(type), content_(content), position_(position) { }
 
     Token(const Type &type, const TokenPosition &position) : type_(type), content_(""), position_(position) { }
 
-    static const char *token_type_name(const Type &type) {
-        switch (type) {
-            case kEOF: return "EOF";
-            case kRead: return "read";
-            case kWrite: return "write";
-            case kWhile: return "while";
-            case kIf: return "if";
-            case kElse: return "else";
-            case kInt: return "int";
-            case kReal: return "real";
-            case kVoid: return "void";
-            case kFunc: return "func";
-            case kReturn: return "return";
-            case kLeftParen: return "(";
-            case kRightParen: return ")";
-            case kLeftBracket: return "[";
-            case kRightBracket: return "]";
-            case kLeftBrace: return "{";
-            case kRightBrace: return "}";
-            case kSemicolon: return ";";
-            case kComma: return ",";
-            case kAssign: return "=";
-            case kPlus: return "+";
-            case kMinus: return "-";
-            case kTimes: return "*";
-            case kDivide: return "/";
-            case kLT: return "<";
-            case kLTE: return "<=";
-            case kGT: return ">";
-            case kGTE: return ">=";
-            case kEqual: return "==";
-            case kNotEqual: return "<>";
-            case kLineComment: return "//";
-            case kLeftBlockComment: return "/*";
-            case kRightBlockComment: return "*/";
-            case kIdentity: return "identity";
-            case kIntegerLiteral: return "integer_literal";
-            case kRealLiteral: return "real_literal";
-            case kWhiteSpace: return "white_space";
-            default: throw std::invalid_argument("received invalid type value");
-        }
+    static const char *token_type_name(const Type &type);
+
+    const char *type_name() const {
+        return token_type_name(type_);
     }
 
-    const char *type_name() const { return token_type_name(type_); }
+    Type type() const {
+        return type_;
+    }
 
-    Type type() const { return type_; }
-    void set_type(Type type) { type_ = type; }
+    void set_type(Type type) {
+        type_ = type;
+    }
 
     std::string content() const {
         if (content_.length() > 0) {
@@ -113,10 +78,18 @@ public:
             return type_name();
         }
     }
-    void set_content(const std::string &content) { content_ = content; }
 
-    TokenPosition position() const { return position_; }
-    void set_position(const TokenPosition &position) { position_ = position; }
+    void set_content(const std::string &content) {
+        content_ = content;
+    }
+
+    TokenPosition position() const {
+        return position_;
+    }
+
+    void set_position(const TokenPosition &position) {
+        position_ = position;
+    }
 
     friend std::ostream &operator << (std::ostream &os, const Token &token) {
         std::cout << "第 " << token.position().row() << " 行 第 " << token.position().col() << " 列:<\"" << token.type_name() << "\", \"" << token.content() << "\">";
---
+++ token.cpp
@@ -0,0 +1,44 @@
+#include "include/token.h"
+
+const char *Token::token_type_name(const Type &type) {
+    switch (type) {
+        case kEOF: return "EOF";
+        case kRead: return "read";
+        case kWrite: return "write";
+        case kWhile: return "while";
+        case kIf: return "if";
+        case kElse: return "else";
+        case kInt: return "int";
+        case kReal: return "real";
+        case kVoid: return "void";
+        case kFunc: return "func";
+        case kReturn: return "return";
+        case kLeftParen: return "(";
+        case kRightParen: return ")";
+        case kLeftBracket: return "[";
+        case kRightBracket: return "]";
+        case kLeftBrace: return "{";
+        case kRightBrace: return "}";
+        case kSemicolon: return ";";
+        case kComma: return ",";
+        case kAssign: return "=";
+        case kPlus: return "+";
+        case kMinus: return "-";
+        case kTimes: return "*";
+        case kDivide: return "/";
+        case kLT: return "<";
+        case kLTE: return "<=";
+        case kGT: return ">";
+        case kGTE: return ">=";
+        case kEqual: return "==";
+        case kNotEqual: return "<>";
+        case kLineComment: return "//";
+        case kLeftBlockComment: return "/*";
+        case kRightBlockComment: return "*/";
+        case kIdentity: return "identity";
+        case kIntegerLiteral: return "integer_literal";
+        case kRealLiteral: return "real_literal";
+        case kWhiteSpace: return "white_space";
+        default: throw std::invalid_argument("received invalid type value");
+    }
+}
