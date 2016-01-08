a95aefb452ca5f6fccbe61c2e6f349213c6295c2
doraemonext <doraemonext@gmail.com>
2016-01-08 23:11:36 +0800

    Del: 删除main.cpp中的无用代码

--- main.cpp
+++ main.cpp
@@ -5,7 +5,6 @@
 #include "include/lexer.h"
 #include "include/parser.h"
 #include "include/semantic.h"
-#include "include/ir.h"
 
 using namespace std;
 
@@ -26,36 +25,10 @@ int main() {
         semantic.analyse();
         cout << endl << "中间代码:" << endl << endl;
         semantic.print_ir();
-        semantic.print_error_messages();
-        semantic.print_warning_messages();
     } catch (const scope_critical_error &e) {
         semantic.print_error_messages();
-        semantic.print_warning_messages();
     }
-
-//    IR ir;
-//    ir.add(PCode(PCode::Type::kStartFunc, "factor", 0));
-//    ir.add(PCode(PCode::Type::kReturnIntegerArray, "a"));
-//    std::cout << ir;
-
-    // std::vector<IR> ir;
-    // ir.push_back(IR(OpCode::kAdd, Var(Var::Type::kInt, 10), Var(Var::Type::kReal, 10.0), Var(Var::Type::kInt, 10)));
-    // cout << ir[0] << endl;
-
-//    ScopeTree *tree = new ScopeTree();
-//
-//    Symbol s1("i", 9);
-//    tree->define(s1);
-//
-//    Symbol s2("f", Symbol::Type::kReal, {Symbol("x", 0), Symbol("y", 0.0)});
-//    tree->define(s2);
-//
-//    tree->push();
-//
-//    Symbol s3("x", 0);
-//    Symbol s4("y", 0.0);
-//    tree->define(s3);
-//    tree->define(s4);
+    semantic.print_warning_messages();
 
     return 0;
 }
